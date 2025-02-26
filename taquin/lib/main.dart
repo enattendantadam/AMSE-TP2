6c05b0f000d35f93e17738305424e5a3fe36e4d4
enattendantadam <160159891+enattendantadam@users.noreply.github.com>
2025-02-26 09:54:27 +0100

    Update main.dart

--- taquin/lib/main.dart
+++ taquin/lib/main.dart
@@ -5,6 +5,66 @@ void main() {
   runApp(const MyApp());
 }
 
+Tile tile =
+    new Tile(imageURL: 'https://picsum.photos/512', alignment: Alignment(0, 0));
+
+class Tile {
+  String imageURL;
+  Alignment alignment;
+
+  Tile({required this.imageURL, required this.alignment});
+
+  Widget croppedImageTile() {
+    return FittedBox(
+      fit: BoxFit.fill,
+      child: ClipRect(
+        child: Container(
+          child: Align(
+            alignment: this.alignment,
+            widthFactor: 0.3,
+            heightFactor: 0.3,
+            child: Image.network(this.imageURL),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DisplayTileWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Display a Tile as a Cropped Image'),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Column(children: [
+        SizedBox(
+            width: 150.0,
+            height: 150.0,
+            child: Container(
+                margin: EdgeInsets.all(20.0),
+                child: this.createTileWidgetFrom(tile))),
+        Container(
+            height: 200,
+            child:
+                Image.network('https://picsum.photos/512', fit: BoxFit.cover))
+      ])),
+    );
+  }
+
+  Widget createTileWidgetFrom(Tile tile) {
+    return InkWell(
+      child: tile.croppedImageTile(),
+      onTap: () {
+        print("tapped on tile");
+      },
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -14,21 +74,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -43,18 +88,45 @@ class ExerciseViewerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Exo1"),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ListView(
-              children: [
-                ListTile(
-                  title: Text("exo1"),
-                  subtitle: Text("explication exo1"),
-                )
-              ],
+            Expanded(
+              child: ListView(
+                children: [
+                  ListTile(
+                    title: Text("exo1"),
+                    subtitle: Text("explication exo1"),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Exo1()));
+                    },
+                  ),
+                  ListTile(
+                    title: Text("exo2"),
+                    subtitle: Text("explication exo2"),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Exo2()));
+                    },
+                  ),
+                  ListTile(
+                    title: Text("exo4"),
+                    subtitle: Text("explication exo4"),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DisplayTileWidget()));
+                    },
+                  ),
+                ],
+              ),
             )
           ],
         ),
@@ -67,7 +139,6 @@ class Exo1 extends StatefulWidget {
   const Exo1({super.key});
   @override
   State<Exo1> createState() => _MyHomePageState();
-
 }
 
 class _MyHomePageState extends State<Exo1> {
@@ -120,7 +191,9 @@ class _MyHomePageState extends State<Exo1> {
         // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[Transform.flip(flipX: _isChecked,
+          children: <Widget>[
+            Transform.flip(
+              flipX: _isChecked,
               child: Transform(
                 alignment: Alignment.center,
                 transform: Matrix4.identity()
@@ -174,6 +247,66 @@ class _MyHomePageState extends State<Exo1> {
                 });
               },
               onChangeEnd: (_) => _stopSound(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Exo2 extends StatefulWidget {
+  const Exo2({super.key});
+
+  @override
+  State<Exo2> createState() => _Exo2State();
+}
+
+class _Exo2State extends State<Exo2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Exo2"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.network(
+              "https://picsum.photos/512/1024",
+              width: 200,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Exo5a extends StatefulWidget {
+  const Exo5a({super.key});
+
+  @override
+  State<Exo5a> createState() => _Exo5aState();
+}
+
+class _Exo5aState extends State<Exo5a> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Exo5a"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.network(
+              "https://picsum.photos/512/1024",
+              width: 200,
             ),
           ],
         ),
