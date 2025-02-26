98473c25a896eeb718e298ea4e019c382f876905
enattendantadam <160159891+enattendantadam@users.noreply.github.com>
2025-02-26 09:30:34 +0100

    ajout exo2

--- taquin/lib/main.dart
+++ taquin/lib/main.dart
@@ -46,6 +46,14 @@ class ExerciseViewerPage extends StatelessWidget {
                       Navigator.push(context,
                           MaterialPageRoute(builder: (context) => Exo1()));
                     },
+                  ),
+                  ListTile(
+                    title: Text("exo2"),
+                    subtitle: Text("explication exo2"),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Exo2()));
+                    },
                   )
                 ],
               ),
@@ -167,6 +175,36 @@ class _MyHomePageState extends State<Exo1> {
                 });
               },
               onChangeEnd: (_) => _stopSound(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Exo2 extends StatefulWidget {
+  const Exo2({super.key});
+
+  @override
+  State<Exo2> createState() => _Exo2State();
+}
+
+class _Exo2State extends State<Exo2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Exo2"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.network(
+              "https://picsum.photos/512/1024",
+              width: 200,
             ),
           ],
         ),
