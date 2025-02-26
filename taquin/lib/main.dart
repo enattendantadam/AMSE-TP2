27b12a33f0da7a75f67b05225f09dcc5f4dc6464
SuzanneLeonore <suzanne.girardjollet@gmail.com>
2025-02-26 09:41:54 +0100

    ajout mirror

--- taquin/lib/main.dart
+++ taquin/lib/main.dart
@@ -5,9 +5,6 @@ void main() {
   runApp(const MyApp());
 }
 
-Tile tile =
-    new Tile(imageURL: 'https://picsum.photos/512', alignment: Alignment(0, 0));
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -17,7 +14,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const ExerciseViewerPage(),
@@ -31,45 +43,18 @@ class ExerciseViewerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Exercices"),
-      ),
+      appBar: AppBar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Expanded(
-              child: ListView(
-                children: [
-                  ListTile(
-                    title: Text("exo1"),
-                    subtitle: Text("explication exo1"),
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Exo1()));
-                    },
-                  ),
-                  ListTile(
-                    title: Text("exo2"),
-                    subtitle: Text("explication exo2"),
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Exo2()));
-                    },
-                  ),
-                  ListTile(
-                    title: Text("exo4"),
-                    subtitle: Text("explication exo4"),
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => DisplayTileWidget()));
-                    },
-                  )
-                ],
-              ),
+            ListView(
+              children: [
+                ListTile(
+                  title: Text("exo1"),
+                  subtitle: Text("explication exo1"),
+                )
+              ],
             )
           ],
         ),
@@ -78,68 +63,11 @@ class ExerciseViewerPage extends StatelessWidget {
   }
 }
 
-class Tile {
-  String imageURL;
-  Alignment alignment;
-
-  Tile({required this.imageURL, required this.alignment});
-
-  Widget croppedImageTile() {
-    return FittedBox(
-      fit: BoxFit.fill,
-      child: ClipRect(
-        child: Container(
-          child: Align(
-            alignment: this.alignment,
-            widthFactor: 0.3,
-            heightFactor: 0.3,
-            child: Image.network(this.imageURL),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class DisplayTileWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Display a Tile as a Cropped Image'),
-        centerTitle: true,
-      ),
-      body: Center(
-          child: Column(children: [
-        SizedBox(
-            width: 150.0,
-            height: 150.0,
-            child: Container(
-                margin: EdgeInsets.all(20.0),
-                child: this.createTileWidgetFrom(tile))),
-        Container(
-            height: 200,
-            child:
-                Image.network('https://picsum.photos/512', fit: BoxFit.cover))
-      ])),
-    );
-  }
-
-  Widget createTileWidgetFrom(Tile tile) {
-    return InkWell(
-      child: tile.croppedImageTile(),
-      onTap: () {
-        print("tapped on tile");
-      },
-    );
-  }
-}
-
 class Exo1 extends StatefulWidget {
   const Exo1({super.key});
-
   @override
   State<Exo1> createState() => _MyHomePageState();
+
 }
 
 class _MyHomePageState extends State<Exo1> {
@@ -192,16 +120,17 @@ class _MyHomePageState extends State<Exo1> {
         // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Transform(
-              alignment: Alignment.center,
-              transform: Matrix4.identity()
-                ..rotateX(_rotateX)
-                ..rotateZ(_rotateZ)
-                ..scale(_scalefactor),
-              child: Image.network(
-                "https://picsum.photos/512/1024",
-                width: 200,
+          children: <Widget>[Transform.flip(flipX: _isChecked,
+              child: Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.identity()
+                  ..rotateX(_rotateX)
+                  ..rotateZ(_rotateZ)
+                  ..scale(_scalefactor),
+                child: Image.network(
+                  "https://picsum.photos/512/1024",
+                  width: 200,
+                ),
               ),
             ),
             SizedBox(height: 20),
@@ -245,36 +174,6 @@ class _MyHomePageState extends State<Exo1> {
                 });
               },
               onChangeEnd: (_) => _stopSound(),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class Exo2 extends StatefulWidget {
-  const Exo2({super.key});
-
-  @override
-  State<Exo2> createState() => _Exo2State();
-}
-
-class _Exo2State extends State<Exo2> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Exo2"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.network(
-              "https://picsum.photos/512/1024",
-              width: 200,
             ),
           ],
         ),
