b80fed3d8c8acf86c4025ab84d0f30f28dd4e3d6
enattendantadam <160159891+enattendantadam@users.noreply.github.com>
2025-02-26 09:25:40 +0100

    exercicse viewer

--- taquin/lib/main.dart
+++ taquin/lib/main.dart
@@ -14,22 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         useMaterial3: true,
       ),
       home: const ExerciseViewerPage(),
@@ -43,18 +28,27 @@ class ExerciseViewerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Exercices"),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ListView(
-              children: [
-                ListTile(
-                  title: Text("exo1"),
-                  subtitle: Text("explication exo1"),
-                )
-              ],
+            Expanded(
+              child: ListView(
+                children: [
+                  ListTile(
+                    title: Text("exo1"),
+                    subtitle: Text("explication exo1"),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Exo1()));
+                    },
+                  )
+                ],
+              ),
             )
           ],
         ),
